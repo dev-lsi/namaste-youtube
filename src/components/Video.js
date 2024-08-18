68c9228c016616daee4b49db9df441492d365e83
LachezarIvanov-git <lachezar.ivanov.dev@gmail.com>
2024-08-18 10:12:59 +0300

    thumbnais added to video component

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,8 +4,13 @@ const Video=({videoData})=>{
     const {viewCount,likeCount,favoriteCount,commentCount}=statistics;
     return (
         <div className="vido-container border-2 basis-1/3">
+            <img alt="thumbnail" src={snippet.thumbnails.medium.url}></img>
             <h1>{snippet.title.split('|')[0]}</h1>
-            <h2>{viewCount}</h2>
+            <h2>"views: "{viewCount}</h2>
+            <h2>"likes: "{likeCount}</h2>
+            <h2>"favorite: "{favoriteCount}</h2>
+            <h2>"comments: "{commentCount}</h2>
+
         </div>
     );
 };
